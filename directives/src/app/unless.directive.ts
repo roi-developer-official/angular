f92116293cdbdb338dfb3d5425c4cfb8b2beb38c
roiazran65@gmail.com <62707934+roi65@users.noreply.github.com>
2020-11-25 11:13:01 +0200

    added a directive topic

---
+++ directives/src/app/unless.directive.ts
@@ -0,0 +1,24 @@
+import { Directive, Input, TemplateRef, ViewContainerRef } from '@angular/core';
+
+@Directive({
+  selector: '[appUnless]'
+})
+export class UnlessDirective {
+  // must share the name with the selector
+  @Input() set appUnless(condition:boolean){
+    if(!condition){
+        this.vcRef.createEmbeddedView(this.templateRef)
+    }
+    else{
+        this.vcRef.clear()
+    }
+
+  } 
+
+  constructor(private templateRef: TemplateRef<any>, private vcRef: ViewContainerRef) {
+
+    
+
+  }
+
+}
