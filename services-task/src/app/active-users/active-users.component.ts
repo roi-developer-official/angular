9e738ec5209bf20347b6667ab9f6989401bcb267
roi <roiazran@outlook.co.il>
2020-11-27 23:21:29 +0200

    added oninit

--- services-task/src/app/active-users/active-users.component.ts
+++ services-task/src/app/active-users/active-users.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { CounterService } from '../counter.service';
 import { UserService } from '../user.service';
 
@@ -7,15 +7,17 @@ import { UserService } from '../user.service';
   templateUrl: './active-users.component.html',
   styleUrls: ['./active-users.component.css']
 })
-export class ActiveUsersComponent {
+export class ActiveUsersComponent implements OnInit {
   users: string[];
   counter:number = 0;
 
 
-  constructor(private userService:UserService,private counterService:CounterService){
-    this.users = userService.activeUsers;
-
+  constructor(private userService:UserService,private counterService:CounterService){}
+  
+  ngOnInit(){
+    this.users = this.userService.activeUsers;
   }
+  
   onSetToInactive(id: number) {
     this.userService.onSetToInactive(id)
     this.counterService.counterEmiter.emit({message:'switch from active',num: ++this.counter});
--- services-task/src/app/inactive-users/inactive-users.component.ts
+++ services-task/src/app/inactive-users/inactive-users.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { CounterService } from '../counter.service';
 import { UserService } from '../user.service';
 
@@ -7,16 +7,18 @@ import { UserService } from '../user.service';
   templateUrl: './inactive-users.component.html',
   styleUrls: ['./inactive-users.component.css']
 })
-export class InactiveUsersComponent {
+export class InactiveUsersComponent implements OnInit {
 
   users: string[];
   counter:number = 0;
 
 
-  constructor(private userService: UserService,private counterService:CounterService){
-    this.users = userService.inactiveUsers;
-  }
+  constructor(private userService: UserService,private counterService:CounterService){}
+  
+  ngOnInit(){
+    this.users = this.userService.inactiveUsers;
 
+  }
   onSetToActive(id: number) {
     this.userService.onSetToActive(id)
     this.counterService.counterEmiter.emit({message:'switch from inactive',num: ++this.counter})
