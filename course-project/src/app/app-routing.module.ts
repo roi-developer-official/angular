4998f1305a6ed65efbd1dc40d0dd430d8cc66ba2
roi65 <62707934+roi65@users.noreply.github.com>
2020-12-11 18:39:05 +0200

    p

--- course-project/src/app/app-routing.module.ts
+++ course-project/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
+import { ShoppingListComponent } from './shopping-list/shopping-list.component';
+import { RecipesComponent } from './recipes/recipes.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path : '', redirectTo: '/recipes'},
+  {path : 'recipes' ,component : RecipesComponent},
+  {path : 'shopping-list' ,component : ShoppingListComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
